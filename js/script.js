76b48655c1d0d8822d6b285ab6094a91433bbf0f
Svitlana <svetlanabelan710@gmail.com>
2025-03-13 19:39:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const htmlNode = document.documentElement;
+
+function changeAttributeLang(params){
+  htmlNode.setAttribute('lang','ukr');
+}
+
+const btnChangeLang = document.querySelector('#btn_lang');
+const bodyBlock = document.querySelector('body');
+
+const textBlock = document.querySelector('.text');
+const textBlocks = document.querySelectorAll('.text');
+const textBlocks2 = document.getElementsByClassName('text');
+
+/*--------------------------- MINIMUM ---------------------------*/
+
+function playListFunction(){
+  const playListBLock = document.querySelector('#songs');
+  
+  // Перевіряємо, чи вже існує ul
+  if (!playListBLock.querySelector('ul')) {
+    const playListUl = document.createElement('ul');
+    playListBLock.appendChild(playListUl);
+
+    let playList = [
+      { author: "LED ZEPPELIN", song: "STAIRWAY TO HEAVEN" },
+      { author: "QUEEN", song: "BOHEMIAN RHAPSODY" },
+      { author: "LYNYRD SKYNYRD", song: "FREE BIRD" },
+      { author: "DEEP PURPLE", song: "SMOKE ON THE WATER" },
+      { author: "JIMI HENDRIX", song: "ALL ALONG THE WATCHTOWER" },
+      { author: "AC/DC", song: "BACK IN BLACK" },
+      { author: "QUEEN", song: "WE WILL ROCK YOU" },
+      { author: "METALLICA", song: "ENTER SANDMAN" }
+    ];
+
+    playList.forEach(song => {
+      const playListLi = document.createElement('li');
+      playListLi.innerText = `Автор пісні: ${song.author}\nПісня: ${song.song}`;
+      playListUl.appendChild(playListLi);
+    });
+  }
+}
+
+
+/*--------------------------- MEDIUM ---------------------------*/
+
+function openModalWindow(){
+  const modalWindow = document.querySelector('#modal');
+  modalWindow.classList.add('show');
+  modalWindow.classList.remove('close');
+}
+
+function closeModalWindow(){
+  const modalWindow = document.querySelector('#modal');
+  modalWindow.classList.add('close');
+  modalWindow.classList.remove('show');
+}
+
+
+
+/*--------------------------- MAXIMUM ---------------------------*/
+
+function trafficLightFunction(){
+  
+}
